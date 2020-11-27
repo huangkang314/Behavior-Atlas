5eecf80e5ce45672517a379833e349577fdedfcf
Kang Huang <huangkang314@gmail.com>
2020-11-27 11:29:38 +0800

    Update addMes2log.m

--- lib/addMes2log.m
+++ lib/addMes2log.m
@@ -1,17 +1,17 @@
-function addMes2log(includetime, newinfo, HBTMesBox, HBTlog, display, showWarnmsgbox, replaceline)
+function addMes2log(includetime, newinfo, BeAMesBox, BeAlog, display, showWarnmsgbox, replaceline)
 % add message to log or message box
 %
 % Input
 %   includetime         - bool variable, whether to display the current time
 %   newinfo             - string variable, message to add
-%   HBTMesBox           - bool variable, whether to display message in HBTMesBox
-%   HBTlog              - bool variable, whether to add massage to log
+%   BeAMesBox           - bool variable, whether to display message in BeAMesBox
+%   BeAlog              - bool variable, whether to add massage to log
 %   display             - bool variable, whether to display in Command Window
 %   showmsgbox          - bool variable, warn massage show in massage box
 %
 % History
 %   modified from ledalab (http://www.ledalab.de/)  -  Kang Huang  (kang.huang@siat.ac.cn), 03-02-2020
-global HBT
+global BeA
 
 if nargin < 7
     replaceline = 0;
@@ -23,28 +23,28 @@ if nargin < 5
     display = 0;
 end
 if nargin < 4
-    HBTlog = 1;
+    BeAlog = 1;
 end
 if nargin < 3
-    HBTMesBox = 1;
+    BeAMesBox = 1;
 end
 
 if includetime
     newinfo = [datestr(now,13),': ',newinfo];
 end
 
-if HBTMesBox
+if BeAMesBox
     if replaceline
-        HBT.GUI.Mes_BoxMes = [{newinfo}; HBT.GUI.Mes_BoxMes(2:end)];
+        BeA.GUI.Mes_BoxMes = [{newinfo}; BeA.GUI.Mes_BoxMes(2:end)];
     else
-        HBT.GUI.Mes_BoxMes = [{newinfo}; HBT.GUI.Mes_BoxMes];
+        BeA.GUI.Mes_BoxMes = [{newinfo}; BeA.GUI.Mes_BoxMes];
     end
-    set(HBT.GUI.Mes_Box, 'String', HBT.GUI.Mes_BoxMes);
+    set(BeA.GUI.Mes_Box, 'String', BeA.GUI.Mes_BoxMes);
     %drawnow;
 end
 
-if HBTlog
-    fid_ll = fopen(fullfile('./HBTlog.txt'), 'a');
+if BeAlog
+    fid_ll = fopen(fullfile('./BeAlog.txt'), 'a');
     fprintf(fid_ll,'%s\r\n', newinfo);
     fclose(fid_ll);
 end
